23277f201fa3333b33fe9de835d4f2571cf43840
JungleBird <118018665+RealJungleBird@users.noreply.github.com>
2025-03-27 11:59:28 +0300

    Add TimerStyles.cs

---
+++ Models/TimerStyles.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TinyTimer.Models;
+
+public static class TimerStyles
+{
+    public static List<TimerStyle> timerStyles;
+    public static TimerStyle CurrentTimerStyle { get; set; } = new TimerStyle();
+}
