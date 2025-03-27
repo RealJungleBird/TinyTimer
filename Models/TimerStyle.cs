846fa493f3d6b2bbecd9259431f05fad3869d8af
JungleBird <118018665+RealJungleBird@users.noreply.github.com>
2025-03-27 11:59:13 +0300

    Update TimerStyle.cs

--- Models/TimerStyle.cs
+++ Models/TimerStyle.cs
@@ -1,15 +1,73 @@
+using System.ComponentModel;
+using Avalonia;
 using Avalonia.Media;
 
 namespace TinyTimer.Models
 {
-    public class TimerStyle
+    public class TimerStyle : INotifyPropertyChanged
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
         public  int BgOpacity { get; set; } = 40;
-        public  int FontSize { get; set; } = 72;
+
+        public CornerRadius cr
+        {
+            get => new CornerRadius(_cornerRadius);
+        }
+        private int _cornerRadius;
+        public int CornerRadius
+        {
+            get => _cornerRadius;
+            set
+            {
+                if (value != _cornerRadius)
+                {
+                    _cornerRadius = value;
+                    OnPropertyChanged(nameof(cr));
+                }
+            }
+        }
+
+        private int _fontSize = 12;
+        public int FontSize
+        {
+            get => _fontSize;
+            set
+            {
+                if (value != _fontSize)
+                {
+                    _fontSize = value;
+                    OnPropertyChanged(nameof(FontSize));
+                }
+            }
+        }
+        
         public  Color BgColor { get; set; } = Color.FromRgb(255, 255, 255);
-        public  Color TextColor { get; set; } = Color.FromRgb(255, 255, 255);
+        
+        private Color _textColor = Colors.White;
+        public Color TextColor
+        {
+            get => _textColor;
+            set
+            {
+                _textColor = value;
+                OnPropertyChanged(nameof(TextColor));
+            }
+        }
+        
+        
         public  Color TextGlowColor { get; set; } = Color.FromRgb(255, 255, 255);
         public  int TextGlowStrength { get; set; } = 0;
+
+        public TimerStyle()
+        {
+            FontSize = 30;
+            CornerRadius = 54;
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
