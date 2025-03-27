2e30b429d48a63167122e18bca0746dc218af7f8
JungleBird <118018665+RealJungleBird@users.noreply.github.com>
2025-03-27 11:48:57 +0300

    Add bindings to TimerWindow

--- ViewModels/TimerWindowViewModel.cs
+++ ViewModels/TimerWindowViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using Avalonia.Media;
+using TinyTimer.Models;
 
 namespace TinyTimer.ViewModels
 {
     public class TimerWindowViewModel : ViewModelBase
     {
-        
+        public TimerStyle timerStyle { get; set; } = TimerStyles.CurrentTimerStyle;
     }
 }
--- Views/TimerWindow.axaml.cs
+++ Views/TimerWindow.axaml.cs
@@ -3,6 +3,8 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using TinyTimer.Models;
+using TinyTimer.ViewModels;
 
 namespace TinyTimer;
 
@@ -11,7 +13,8 @@ public partial class TimerWindow : Window
     public TimerWindow()
     {
         InitializeComponent();
-        
+        this.DataContext = new TimerWindowViewModel();
+        // (this.DataContext as TimerWindowViewModel).timerStyle = timerStyle;
     }
 
     private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
