662eed0bb2018c361e46ab1136decf8e3c4c46e9
JungleBird <118018665+RealJungleBird@users.noreply.github.com>
2025-05-20 14:21:52 +0300

    Add basic resize border

--- Views/TimerWindow.axaml.cs
+++ Views/TimerWindow.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Media;
 using TinyTimer.ViewModels;
 
 namespace TinyTimer;
@@ -11,10 +12,41 @@ public partial class TimerWindow : Window
         InitializeComponent();
         var viewmodel = new TimerWindowViewModel();
         this.DataContext = viewmodel;
+        
+        // Subscribe to PointerPressed events for all borders
+        LeftBorder.PointerPressed += (s, e) => BeginResize(WindowEdge.West, e);
+        RightBorder.PointerPressed += (s, e) => BeginResize(WindowEdge.East, e);
+        TopBorder.PointerPressed += (s, e) => BeginResize(WindowEdge.North, e);
+        BottomBorder.PointerPressed += (s, e) => BeginResize(WindowEdge.South, e);
+        TopLeftCorner.PointerPressed += (s, e) => BeginResize(WindowEdge.NorthWest, e);
+        TopRightCorner.PointerPressed += (s, e) => BeginResize(WindowEdge.NorthEast, e);
+        BottomLeftCorner.PointerPressed += (s, e) => BeginResize(WindowEdge.SouthWest, e);
+        BottomRightCorner.PointerPressed += (s, e) => BeginResize(WindowEdge.SouthEast, e);
+    }
+
+    private void BeginResize(WindowEdge edge, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            BeginResizeDrag(edge, e);
+            e.Handled = true;
+        }
     }
 
     private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
         BeginMoveDrag(e);
+    }
+
+    protected override void OnPointerEntered(PointerEventArgs e)
+    {
+        base.OnPointerEntered(e);
+        ResizeBorder.BorderBrush = Brushes.DodgerBlue;
+    }
+    
+    protected override void OnPointerExited(PointerEventArgs e)
+    {
+        base.OnPointerExited(e);
+        ResizeBorder.BorderBrush = Brushes.Transparent;
     }
 }
